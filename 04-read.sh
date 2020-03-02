1d8a8f4765350704a58c5b5de1c3a56d81731166
Rajani y <ryhub42@gmail.com>
2020-03-02 13:01:17 -0500

    1

---
+++ 04-read.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -p 'Enter the username to add: ' user_name
+read -p 'Enter the group toadd: ' group_name
+
+echo Following command will be executed => useradd -g $group_name $user_name
