9137e8adbb7b70890c82af731a64e3c3452a81f0
Rajani y <ryhub42@gmail.com>
2020-03-02 12:18:09 -0500

    6

--- 02-print.sh
+++ 02-print.sh
@@ -10,5 +10,5 @@ echo -e Hello\nBye
 echo -e "Hello\nBye"
 echo -e "Hello\tand\tBye"
 echo -e "\e[31mThis text will print in red"
-echo -e "\e[32m This text is supposed to print in Yellow Color"
-echo -e "\e[44mBlue Background color"
+echo -e "\e[33m This text is supposed to print in Yellow Color"
+echo -e "\e[44mBlue Background color\e[0m"
