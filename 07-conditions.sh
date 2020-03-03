9473a317f4e947708831402e6711264db4109af1
Rajani y <ryhub42@gmail.com>
2020-03-03 14:54:50 -0500

    1

---
+++ 07-conditions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ACTION =$1
+SERVICE_NAME=DEMO
+
+START_FUNC() {
+    cho "Starting $SERVICE_NAME Service"
+}
+
+STOP_FUNC() {
+    cho "Stopping $SERVICE_NAME Service"
+}
+
+case $ACTION in
+    start)
+        START_FUNC
+        ;;
+    stop)
+        STOP_FUNC
+        ;;
+    restart)
+        STOP_FUNC
+        START_FUNC
+        ;;
+    *)
+    echo -e "\n\e[33mUsage: $0 action(start|stop|restart)"
+    exit 1
+    
